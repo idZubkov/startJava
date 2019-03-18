e050c222be79e0477f8a4e41c2a5392b6b2dc238
Andrey Zubkov <zubkoff.id@gmail.com>
2019-03-18 14:03:40 +0300

    GuessNumber and GuessNumberTest with mistakes

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,23 +1,36 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
+    private int number;
+    private Player player1;
+    private Player player2;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 
     public void play() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Игра началась!");
+        System.out.println("Игрок 1 делает ход");
+        player1.setNumber(scanner.nextInt());
+        System.out.println("Игрок 2 делает ход");
+        player2.setNumber(scanner.nextInt());
         Random random = new Random();
         int compValue = random.nextInt(100);
-        int number = 0;
+
         while (compValue != number) {
             if (number > compValue) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
                 break;
-            } else if (number < compValue){
+            } else if (number < compValue) {
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
                 break;
-            }
-            else {
+            } else {
                 System.out.println("Вы угадали!");
             }
         }
-
     }
 }
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -3,21 +3,11 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите имя первого игрока: ");
         Player player1 = new Player(scanner.nextLine());
-
         System.out.println("Введите имя второго игрока: ");
         Player player2 = new Player(scanner.nextLine());
-
-        GuessNumber guessNumber = new GuessNumber();
-        System.out.println("Игра началась!");
-        System.out.println("Игрок 1 делает ход" );
-        player1.setNumber(scanner.nextInt());
-        guessNumber.play();
-
-        System.out.println("Игрок 2 делает ход");
-        player2.setNumber(scanner.nextInt());
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
         guessNumber.play();
     }
 }
