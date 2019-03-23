1c35bd5f542c7d3144c7c55489d3127147b986e5
Andrey Zubkov <zubkoff.id@gmail.com>
2019-03-23 18:35:19 +0300

    changed loops do-while and its conditions

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -5,8 +5,10 @@ public class GuessNumberTest {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имя первого игрока: ");
         Player player1 = new Player(scanner.nextLine());
+
         System.out.println("Введите имя второго игрока: ");
         Player player2 = new Player(scanner.nextLine());
+
         GuessNumber guessNumber = new GuessNumber(player1, player2);
         guessNumber.play();
     }
